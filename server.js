193cf671974c63f507a4936bc802663eb952cc4f
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-11-25 11:19:52 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const request = require('request');
+require('dotenv').config();
+
+const app = express();
+
+const apiKey = process.env.API_KEY;
+const PORT = process.env.PORT
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { weather: null, error: null });
+});
+
+app.post('/', (req, res) => {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  console.log(req.body.city)
+  request(url, function (err, response, body) {
+    if (err) {
+      res.render('index', { weather: null, error: 'Error, please try again' });
+    } else {
+      let weather = JSON.parse(body)
+      if (weather.main == undefined) {
+        res.render('index', { weather: null, error: 'Error, please try again' });
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees with ${weather.weather[0].main} in ${weather.name}!`;
+        res.render('index', { weather: weatherText, error: null });
+        console.log("body:", body)
+      }
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Weatherly app is listening o port: ${PORT}`)
+});
